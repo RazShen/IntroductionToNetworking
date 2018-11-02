6049476af46bc09b5d7d85817406be18c440902b
Raz Shenkman <raz.shenkman@mbp-172843.local>
2018-11-02 14:45:44 +0200

    hi

---
+++ Exercise1/Question3/client.py
@@ -0,0 +1,16 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+import sys
+
+if (len(sys.argv)) < 3:
+    print("not enough arguments, quitting...")
+server_ip = sys.argv[1]
+server_port = sys.argv[2]
+
+my_socket = socket(AF_INET, SOCK_DGRAM)
+domain = raw_input("")
+while True:
+    my_socket.sendto(domain, (server_ip, server_port))
+    ip, sender_info = my_socket.recvfrom(2048)
+    print ip
+    domain = raw_input("")
+my_socket.close()
