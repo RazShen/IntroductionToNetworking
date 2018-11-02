86f1807625a1847f73715c0350804d09b442ce4e
Raz Shenkman <raz.shenkman@mbp-172843.local>
2018-11-02 15:06:59 +0200

    new files

--- Exercise1/Question3/server.py
+++ Exercise1/Question3/server.py
@@ -1,13 +1,19 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import sys
 
+# Checking valid input
 if len(sys.argv) < 5:
     print("Not enough arguments, quitting...")
+
+# Parsing the input
 my_port = int(sys.argv[1])
 parent_ip = sys.argv[2]
 parent_port = int(sys.argv[3])
 ips_file_name = sys.argv[4]
 domain_to_ip = {}
+
+# Initializing the dictionary using the ips file
+#  (dictionary is key: domain_name, value: ip_address)
 try:
     with open(ips_file_name) as ips_file:
         for line in ips_file:
@@ -16,16 +22,16 @@ try:
             name, ip = line.split(",")
             ip = ip.strip()
             domain_to_ip[name] = ip
-
-except:
+except Exception:
     print("error in loading ips file, quitting...")
     exit()
 
+# Creating a socket and binding to it using our ip
 my_socket = socket(AF_INET, SOCK_DGRAM)
 my_ip = '127.0.0.1'
 my_socket.bind((my_ip, my_port))
 
-
+# Asking the parent for a specific domain
 def request_from_parent(domain):
     temporary_socket = socket(AF_INET, SOCK_DGRAM)
     temporary_socket.sendto(domain, (parent_ip, parent_port))
@@ -33,15 +39,15 @@ def request_from_parent(domain):
     temporary_socket.close()
     return result
 
+# After receiving from the parent the new domain ip address, write it to the ips file
 def flush():
     with open(ips_file_name, 'w+') as ips:
         for key,value in domain_to_ip.items():
             ips.write(key+"," + value + "\n")
         
-
+# Server always running receving queries from clients.
 while True:
     domain, sender_info = my_socket.recvfrom(2048)
-    print("in line 45")
     if domain in domain_to_ip:
         address = domain_to_ip[domain]
     else:
