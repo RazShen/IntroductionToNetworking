ec4e9d7741dd856c3aaee0ce58bcdd4b7eb7dfbb
ohadshachor123 <ohadshachor123@gmail.com>
2018-12-02 11:05:08 +0200

    fdsfd

--- Exercise2/Question2/client_2.py
+++ Exercise2/Question2/client_2.py
@@ -1,7 +1,7 @@
-import socket, threading
+import socket, time
 
-IP = '127.0.0.1'
-PORT = 12345
+IP = '172.19.3.40'
+PORT = 12347
 RECEIVING_SIZE = 4096
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((IP, PORT))
@@ -9,7 +9,7 @@ for i in range(11):
     msg = 'A'
     socket.send(msg)
     socket.send(msg)
-    threading._sleep(2)
+    time.sleep(2)
 data = socket.recv(RECEIVING_SIZE)
 print "Server sent: ", data
 socket.close()
--- Exercise2/Question2/server_2.py
+++ Exercise2/Question2/server_2.py
@@ -2,7 +2,7 @@ import socket, threading, time
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = '0.0.0.0'
+server_ip = '127.0.0.1'
 server_port = 12345
 server.bind((server_ip, server_port))
 server.listen(5)
